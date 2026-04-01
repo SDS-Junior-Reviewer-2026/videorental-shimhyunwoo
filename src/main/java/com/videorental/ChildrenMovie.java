8efc44fc988ca6ed269e1fbb0771049276f03abb
Data-MaSTeRR <hyunwoo8504@gmail.com>
2026-04-01 17:00:30 +0900

    Refactor: Make Movie class abstract and create specific subclasses for pricing logic

---
+++ src/main/java/com/videorental/ChildrenMovie.java
@@ -0,0 +1,23 @@
+package com.videorental;
+
+public class ChildrenMovie extends Movie {
+    public ChildrenMovie(String title) {
+        super(title, Movie.CHILDRENS);
+    }
+
+    @Override
+    public double getChargeFor(int daysRented) {
+        double thisAmount = 0;
+        thisAmount += 1.5;
+        if (daysRented > 3)
+            thisAmount += (daysRented - 3) * 1.5;
+
+        return thisAmount;
+    }
+
+    @Override
+    int getFrequentRenterPointsFor(int daysRented) {
+        return 1;
+    }
+}
+
